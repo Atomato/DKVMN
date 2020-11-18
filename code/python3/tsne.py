d439be81bc73c6fb60afc99fed8bb902049a91fa
keunhyung_chung <keunhyung_chung@tmax.co.kr>
2020-11-18 07:45:36 +0000

    Update tsne.py

--- code/python3/tsne.py
+++ code/python3/tsne.py
@@ -19,12 +19,15 @@ for idx, line in enumerate(lines):
         data.append(np.array(list(map(lambda s: float(s), line.strip().split(',')))))
 data = np.array(data)
 
-label = []
-for d in data:
-    label.append(np.argmax(d))
+# label = []
+# for d in data:
+#     label.append(np.argmax(d))
 
-label = np.array(label)
-print(label)
+# label = np.array(label)
+# print(label)
+
+# Ground truth
+label = [3, 2, 3, 2, 3, 4, 0, 2, 3, 0, 1, 0, 1, 4, 3, 3, 0, 1, 4, 4, 0, 4, 4, 1, 1, 0, 0, 4, 2, 0, 4, 4, 1, 2, 4, 0, 3, 0, 1, 2, 2, 0, 3, 3, 2, 3, 2, 3, 2, 1]
 
 model = TSNE(n_components=2)
 transformed = model.fit_transform(data)
@@ -35,6 +38,8 @@ ys = transformed[:,1]
 # %%
 plt.scatter(xs,ys,c=label)
 
+for i in range(n_question):
+    plt.text(xs[i], ys[i], i+1)
 plt.show()
 # %%
 cluster = {0: [], 1: [], 2: [], 3: [], 4: []}
@@ -43,4 +48,3 @@ for i, v in enumerate(label):
 
 for k in cluster:
     print(cluster[k])
-# %%
